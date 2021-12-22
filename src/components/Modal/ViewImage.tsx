44a236d892e5489f9d68afb4f4a136686327806b
Rellyson Douglas <rellysondouglas2015@gmail.com>
2021-12-22 07:51:50 -0300

    Created image view modal

--- src/components/Modal/ViewImage.tsx
+++ src/components/Modal/ViewImage.tsx
@@ -20,4 +20,30 @@ export function ModalViewImage({
   imgUrl,
 }: ModalViewImageProps): JSX.Element {
   // TODO MODAL WITH IMAGE AND EXTERNAL LINK
+  return (
+    <Modal isOpen={isOpen} onClose={onClose}>
+      <ModalOverlay />
+      <ModalContent
+        w="fit-content"
+        h="fit-content"
+        maxW={900}
+        maxH={600}
+      >
+        <ModalBody
+          p="0"
+        >
+          <Image
+            src={imgUrl}
+            w="auto"
+            h="fit-content"
+            maxW={900}
+            maxH={600}
+          />
+        </ModalBody>
+        <ModalFooter bgColor="pGray.800" justifyContent="flex-start">
+          <Link isExternal href={imgUrl}>Abrir original</Link>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  )
 }
